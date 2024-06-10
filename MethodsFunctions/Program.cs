2e0a4ee2ba2a7005e1b1dd8a0fcc5ae74f7f7408
CameronAce007 <CNkomo@ITLC7DKQV2>
2024-06-10 10:33:34 +0200

    created salary app class

---
+++ MethodsFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
